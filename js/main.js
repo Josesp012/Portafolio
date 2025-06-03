19c97833a0d185dff9590b1a8cd7a0e103adfbc9
JoseSp <jose.salazarp_9612@hotmail.com>
2025-06-02 22:06:24 -0600

    modificacion navbar y leyenda js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ const oscuro=document.getElementById('oscuro');
 let clima=document.getElementById('clima');
 let tema=document.getElementById('tema');
 const claro=document.getElementById('claro');
-let nube=document.getElementById('nube');
+let nube1=document.getElementById('nube1');
+let nube2=document.getElementById('nube2');
+let nube3=document.getElementById('nube3');
+let nube4=document.getElementById('nube4');
+let estrella1=document.getElementById('estrella1');
+let estrella2=document.getElementById('estrella2');
+let estrella3=document.getElementById('estrella3');
+let estrella4=document.getElementById('estrella4');
 
-function temaOscuro() {
-  let estrella1=document.getElementById('estrella1');
-  let estrella2=document.getElementById('estrella2');
-  let estrella3=document.getElementById('estrella3');
-  let estrella4=document.getElementById('estrella4');
+function temaOscuro() {  
   tema.innerHTML='<img src="https://cdn-icons-png.flaticon.com/512/1518/1518052.png" alt="luna">';
   document.body.style.backgroundColor="black";
   clima.style.backgroundColor="white";
@@ -18,7 +21,10 @@ function temaOscuro() {
   estrella2.style.color ="yellow";
   estrella3.style.color ="yellow";
   estrella4.style.color ="yellow";
-  nube.style.backgroundColor="black";
+  nube1.style.backgroundColor="black";
+  nube2.style.backgroundColor="black";
+  nube3.style.backgroundColor="black";
+  nube4.style.backgroundColor="black";
 }
 function temaClaro() {
    tema.innerHTML='<img src="https://cdn-icons-png.flaticon.com/512/6661/6661565.png" alt="sol">';
@@ -33,6 +39,10 @@ function idiomaIngles(){
   let soy=document.getElementById('soy');
   ingles.innerHTML='<input id="ingles" type="radio" name="account-type" checked onclick="idiomaIngles(this)"/> English';
   soy.innerHTML='Web/Java Developer';
+  estrella1.innerHTML="Education";
+  estrella2.innerHTML="Skills";
+  estrella3.innerHTML="Projects";
+  estrella4.innerHTML="Contact us";
 }
 
 function idiomaEspañol(){
