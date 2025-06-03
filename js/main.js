de3fc091b80c5e28bd8e09305d14a764238e2edc
JoseSp <jose.salazarp_9612@hotmail.com>
2025-06-02 21:45:31 -0600

    Se agrego mas estilos de eventos al tema oscuro

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
+const oscuro=document.getElementById('oscuro');
+let clima=document.getElementById('clima');
+let tema=document.getElementById('tema');
+const claro=document.getElementById('claro');
+let nube=document.getElementById('nube');
+
 function temaOscuro() {
-    const oscuro=document.getElementById('oscuro');
-    let clima=document.getElementById('clima');
-    let tema=document.getElementById('tema');
-    const claro=document.getElementById('claro');
-    tema.innerHTML='<img src="https://cdn-icons-png.flaticon.com/512/1518/1518052.png" alt="luna">';
-    document.body.style.backgroundColor="black";
-    clima.style.backgroundColor="white";
-    oscuro.style.display="none";
-    claro.style.display="block";
+  let estrella1=document.getElementById('estrella1');
+  let estrella2=document.getElementById('estrella2');
+  let estrella3=document.getElementById('estrella3');
+  let estrella4=document.getElementById('estrella4');
+  tema.innerHTML='<img src="https://cdn-icons-png.flaticon.com/512/1518/1518052.png" alt="luna">';
+  document.body.style.backgroundColor="black";
+  clima.style.backgroundColor="white";
+  oscuro.style.display="none";
+  claro.style.display="block";
+  estrella1.style.color ="yellow";
+  estrella2.style.color ="yellow";
+  estrella3.style.color ="yellow";
+  estrella4.style.color ="yellow";
+  nube.style.backgroundColor="black";
 }
 function temaClaro() {
-  const oscuro=document.getElementById('oscuro');
-  let clima=document.getElementById('clima');
-  let tema=document.getElementById('tema');
-  const claro=document.getElementById('claro');
-  tema.innerHTML='<img src="https://cdn-icons-png.flaticon.com/512/6661/6661565.png" alt="sol">';
-  document.body.style.backgroundColor="#3fc0e7b8";
+   tema.innerHTML='<img src="https://cdn-icons-png.flaticon.com/512/6661/6661565.png" alt="sol">';
+   document.body.style.backgroundColor="#3fc0e7b8";
   clima.style.backgroundColor="yellow";
   oscuro.style.display="block";
   claro.style.display="none";
